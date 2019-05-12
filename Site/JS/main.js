fada364ad792d68be206b57dc46f6b1fd90d6839
Thomas Nguyen <thomas.nguyen@learnet.se>
2019-05-13 00:04:47 +0200

    inte klar och om du kan, kolla på min på slutet av måndag. VIll bara länka sidorna och skicka in en video som går igenom det jag har gjort

---
+++ Site/JS/main.js
@@ -0,0 +1,8 @@
+function buy_something(event){
+    let ordered = document.querySelector(".ordered")
+    ordered.classList.toggle("show")
+    
+    setTimeout(function () {let ordered = document.querySelector(".ordered")
+    ordered.classList.toggle("show")},3000)
+    
+}
